b66d7a89d27e0e4b98913e07c847103a43c1b155
KKitty <131541729+KKittyy@users.noreply.github.com>
2025-09-13 13:26:54 +0300

    ed

---
+++ 2L2/script.js
@@ -0,0 +1,25 @@
+let cutie = "picuri";
+var x = 10;
+const data = "12.09.2020";
+let confirmare = false;
+
+x = 500;
+cutie = 125;
+// data = "15.09.2010";
+
+
+
+//sarcin 1 cred
+
+let n = Math.floor(Math.random()*101);
+console.log("lature patratului este "+ n + "cm")
+console.log("Aria patratului este "+ n*n + "cm^2")
+
+prompt("john")
+
+//sarci 2'
+
+let numar = Number(prompt("inroftudcrit numar:"))
+console.log("nmar introdus: "+numar)
+console.log(`Urmatorul numar: ${numar+1}`)
+console.log(`Urmatorul numar: ${numar+2}`)
